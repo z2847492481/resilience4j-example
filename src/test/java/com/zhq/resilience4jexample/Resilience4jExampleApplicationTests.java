cab104e9ce5d43a74808ea1d827ef9ccdd1bad94
zhq123 <2847492481@qq.com>
2024-03-11 22:11:41 +0800

    init

---
+++ src/test/java/com/zhq/resilience4jexample/Resilience4jExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhq.resilience4jexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
